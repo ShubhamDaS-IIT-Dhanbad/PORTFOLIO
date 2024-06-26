5a34fbe5cd37cc374cd55ee0b80aa21983707619
ShubhamDaS-IIT-Dhanbad <shubhamdasiitcs@gmail.com>
2024-06-26 16:03:59 +0530

    first

--- src/utils/typeAnimation.jsx
+++ src/utils/typeAnimation.jsx
@@ -7,30 +7,34 @@ const TypeAnimation = ({ sequence, wrapper, repeat }) => {
 
   useEffect(() => {
     let intervalId;
+
     if (isTyping) {
       const currentText = sequence[currentIndex];
       intervalId = setInterval(() => {
-        setText(prevText => {
-          if (prevText === currentText) {
-            clearInterval(intervalId);
-            setTimeout(() => setIsTyping(false), 3000); // Wait 3 seconds before erasing
-            return prevText;
-          }
-          return currentText.substring(0, prevText.length + 1);
-        }, 60); // Typing speed
-      });
+        setText(prevText =>
+          prevText.length === currentText.length
+            ? (() => {
+                clearInterval(intervalId);
+                setTimeout(() => setIsTyping(false), 3000);
+                return prevText;
+              })()
+            : currentText.substring(0, prevText.length + 1)
+        );
+      }, 60);
     } else {
+      const currentText = sequence[currentIndex];
       intervalId = setInterval(() => {
-        setText(prevText => {
-          if (prevText === '') {
-            clearInterval(intervalId);
-            setCurrentIndex((prevIndex) => (prevIndex + 1) % sequence.length); // Move to next sequence
-            setIsTyping(true);
-            return prevText;
-          }
-          return prevText.substring(0, prevText.length - 1);
-        }, 50); // Erasing speed
-      });
+        setText(prevText =>
+          prevText.length === 0
+            ? (() => {
+                clearInterval(intervalId);
+                setCurrentIndex(prevIndex => (prevIndex + 1) % sequence.length);
+                setIsTyping(true);
+                return prevText;
+              })()
+            : currentText.substring(0, prevText.length - 1)
+        );
+      }, 50);
     }
 
     return () => clearInterval(intervalId);
