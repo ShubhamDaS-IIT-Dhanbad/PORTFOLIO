e44c35dc817d89a483504166586a13cd80ef5c3d
ShubhamDaS-IIT-Dhanbad <shubhamdasiitcs@gmail.com>
2024-06-26 21:58:52 +0530

    first

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -8,28 +8,25 @@ import { FaLinkedin, FaFacebookSquare, FaInstagramSquare, FaTwitterSquare, FaGit
 
 function Home() {
   const sequence = [
-    'Frontend Developer',
+    'Frontend Developer','Software Developer','Frontend Developer',
+    'Backend Developer','Software Developer','Frontend Developer',
     'Backend Developer',
     'MERN Developer',
-    'Software Developer',
-    'Frontend Developer',
+    'Software Developer','Frontend Developer',
     'Backend Developer',
     'MERN Developer',
-    'Software Developer',
-    'Frontend Developer',
+    'Software Developer','Frontend Developer',
     'Backend Developer',
     'MERN Developer',
-    'Software Developer',
-    'Frontend Developer',
+    'Software Developer','Frontend Developer',
     'Backend Developer',
     'MERN Developer',
-    'Software Developer',
-    'Frontend Developer',
+    'Software Developer','Frontend Developer',
     'Backend Developer',
     'MERN Developer',
-    'Software Developer',
-    'Frontend Developer',
+    'Software Developer','Frontend Developer',
     'Backend Developer',
+    'MERN Developer',
     'MERN Developer',
     'Software Developer',
     'Backend Developer',
@@ -48,18 +45,9 @@ function Home() {
     'MERN Developer',
     'Software Developer',
     'Frontend Developer',
-    'Backend Developer',
+   
     'MERN Developer',
-    'Software Developer',
-    'Frontend Developer',
-    'Backend Developer',
-    'MERN Developer',
-    'Software Developer',
-    'Backend Developer',
-    'MERN Developer',
-    'Software Developer',
-    'Frontend Developer',
-    'Backend Developer',
+    'Software Developer','Frontend Developer', 'Backend Developer',
     'MERN Developer',
     'Software Developer',
     'Frontend Developer',
@@ -69,18 +57,14 @@ function Home() {
     'Frontend Developer',
     'Backend Developer',
     'MERN Developer',
-    'Software Developer',
-    'Frontend Developer',
+    
     'Backend Developer',
     'MERN Developer',
-    'Software Developer',
-    'Frontend Developer',
+    'Software Developer','Frontend Developer',
     'Backend Developer',
     'MERN Developer',
-    'Software Developer',
-    'Backend Developer',
-    'MERN Developer',
-    'Software Developer',
+    'Software Developer','Frontend Developer',
+    'Backend Developer','Software Developer',
     'Frontend Developer',
     'Backend Developer',
     'MERN Developer',
@@ -92,39 +76,46 @@ function Home() {
     'Frontend Developer',
     'Backend Developer',
     'MERN Developer',
-    'Software Developer',
-    'Frontend Developer',
+    'Software Developer','Frontend Developer',
     'Backend Developer',
     'MERN Developer',
-    'Software Developer',
-    'Frontend Developer',
+    'Software Developer','Frontend Developer','Backend Developer','MERN Developer',
+    'Software Developer','Frontend Developer',
     'Backend Developer',
     'MERN Developer',
-    'Software Developer',
+    'Software Developer','Frontend Developer',
+    'Backend Developer',
+    'MERN Developer',
+    'Software Developer','Frontend Developer',
+    'Backend Developer',
+    'MERN Developer',
+    
+    
   ];
+
   const navigate = useNavigate();
 
   const handleContactClick = () => {
-    navigate('/contact'); // Update this path to match your contact page route
+    navigate('/contact');
   };
-  const handleHireClick = () => {
-    navigate('/contact'); // Update this path to match your contact page route
-  };
+
   const handleLinkedInClick = () => {
-    navigate('/www.linkedin.com/in/shubham-das-22je0936'); // Update this path to match your contact page route
+    window.open('https://www.linkedin.com/in/shubham-das-22je0936', '_blank');
   };
+
   const handleInstaClick = () => {
-    navigate('/https://www.instagram.com/shubhdasham');
+    window.open('https://www.instagram.com/shubhdasham', '_blank');
   };
+
   const handleTwitterClick = () => {
-    alert("ABHI TO NHI HAI KHOL LENGE :)")
+    // Handle Twitter click action
+    alert("TERE LYA KHOL NA PDEGA :)");
   };
+
   const handleGitClick = () => {
-    navigate('/https://github.com/ShubhamDaS-IIT-Dhanbad'); // Update this path to match your contact page route
+    window.open('https://github.com/ShubhamDaS-IIT-Dhanbad', '_blank');
   };
-  const handleFaceBookClick = () => {
-    alert("ABHI TO NHI HAI KHOL LENGE :)")
-  };
+
   return (
     <div className='home'>
       <div className='home-left'>
@@ -133,7 +124,7 @@ function Home() {
         </div>
         <div className='home-left-2'>
           I'm a <div className='home-left-2-title'>
-          <TypeAnimation sequence={sequence} wrapper="div" repeat={Infinity} />
+            <TypeAnimation sequence={sequence} wrapper="div" repeat={Infinity} />
           </div>
         </div>
         <p className='home-left-3'>
@@ -143,18 +134,18 @@ function Home() {
           unwavering dedication, I am determined to leave an indelible mark in the dynamic realm of web development.
         </p>
         <div className='home-left-4'>
-          <FaLinkedin className='footer-icons-div' onClick={handleLinkedInClick}/>
-          <FaFacebookSquare className='footer-icons-div' onClick={handleFaceBookClick}/>
-          <FaInstagramSquare className='footer-icons-div'onClick={ handleInstaClick} />
-          <FaTwitterSquare className='footer-icons-div' onClick={handleTwitterClick}/>
-          <FaGithubSquare className='footer-icons-div' onClick={handleGitClick }/>
+          <FaLinkedin className='footer-icons-div' onClick={handleLinkedInClick} />
+          <FaFacebookSquare className='footer-icons-div' onClick={() => alert("This feature is coming soon!")} />
+          <FaInstagramSquare className='footer-icons-div' onClick={handleInstaClick} />
+          <FaTwitterSquare className='footer-icons-div' onClick={handleTwitterClick} />
+          <FaGithubSquare className='footer-icons-div' onClick={handleGitClick} />
         </div>
         <div className='home-left-5'>
-      <div className='home-left-5-1' onClick={handleContactClick}>Hire</div>
-      <div className='home-left-5-2' onClick={handleContactClick}>Contact</div>
-    </div>
+          <div className='home-left-5-1' onClick={handleContactClick}>Hire</div>
+          <div className='home-left-5-2' onClick={handleContactClick}>Contact</div>
+        </div>
       </div>
-      
+
       <div className='home-right'>
         <img src={logo} alt="Logo" />
       </div>
