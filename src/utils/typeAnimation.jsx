362533d6bd7be4948285d2ae546a0d159db2324b
ShubhamDaS-IIT-Dhanbad <shubhamdasiitcs@gmail.com>
2024-06-26 16:01:55 +0530

    first

--- src/utils/typeAnimation.jsx
+++ src/utils/typeAnimation.jsx
@@ -8,30 +8,29 @@ const TypeAnimation = ({ sequence, wrapper, repeat }) => {
   useEffect(() => {
     let intervalId;
     if (isTyping) {
+      const currentText = sequence[currentIndex];
       intervalId = setInterval(() => {
-        const currentText = sequence[currentIndex];
-        setText((prevText) => {
-          if (prevText.length === currentText.length) {
+        setText(prevText => {
+          if (prevText === currentText) {
             clearInterval(intervalId);
             setTimeout(() => setIsTyping(false), 3000); // Wait 3 seconds before erasing
             return prevText;
           }
           return currentText.substring(0, prevText.length + 1);
-        });
-      }, 60); // Typing speed
+        }, 60); // Typing speed
+      });
     } else {
       intervalId = setInterval(() => {
-        const currentText = sequence[currentIndex];
-        setText((prevText) => {
-          if (prevText.length === 0) {
+        setText(prevText => {
+          if (prevText === '') {
             clearInterval(intervalId);
             setCurrentIndex((prevIndex) => (prevIndex + 1) % sequence.length); // Move to next sequence
             setIsTyping(true);
             return prevText;
           }
-          return currentText.substring(0, prevText.length - 1);
-        });
-      }, 50); // Erasing speed
+          return prevText.substring(0, prevText.length - 1);
+        }, 50); // Erasing speed
+      });
     }
 
     return () => clearInterval(intervalId);
