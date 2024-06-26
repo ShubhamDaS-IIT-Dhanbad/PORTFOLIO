8792d27be3dcb01e3c58006aab45e2a4a7c40253
ShubhamDaS-IIT-Dhanbad <shubhamdasiitcs@gmail.com>
2024-06-26 15:57:50 +0530

    first

--- src/pages/services.jsx
+++ src/pages/services.jsx
@@ -1,43 +1,43 @@
+// Services.jsx
 import React from 'react';
 import '../styles/services.css';
-
 import { CiLocationArrow1 } from "react-icons/ci";
 
 function Services() {
     return (
         <div className='services'>
-            <div className='services-div'>
-                <div className='services-div-1'>
-                    <div className='services-div-1-1'>
-                        <div className='services-div-1-1-1'>
-                            01
-                             <CiLocationArrow1 size={30} className='services-div-1-arrow'/>
-                        </div>
-                        <div className='services-div-1-1-2'>WEB DEVELOPMENT</div>
-                        <p>Lorem ipsum dolor sit amet consectetur, iusto corporis at sunt officia! Itaque, deleniti qui!</p>
+            <div className='services-grid'>
+                <div className='services-item'>
+                    <div className='services-item-header'>
+                        01
+                        <CiLocationArrow1 size={30} className='services-item-arrow' />
                     </div>
-                    <div className='services-div-1-1'>
-                        <div className='services-div-1-1-1'>02 <CiLocationArrow1 size={30} className='services-div-1-arrow'/>
-                        </div>
-                        <div className='services-div-1-1-2'>BACKEND DEVELOPER</div>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, facere!</p>
-                    </div>
+                    <div className='services-item-title'>WEB DEVELOPER</div>
+                    <p>Lorem ipsum dolor sit amet consectetur, iusto corporis at sunt officia! Itaque, deleniti qui!</p>
                 </div>
-                <div className='services-div-1'>
-                    <div className='services-div-1-1'>
-                        <div className='services-div-1-1-1'>03 
-                             <CiLocationArrow1 size={30} className='services-div-1-arrow'/>
-                        </div>
-                        <div className='services-div-1-1-2'>COMPETITIVE PROGRAMMER</div>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, facere!</p>
+                <div className='services-item'>
+                    <div className='services-item-header'>
+                        02
+                        <CiLocationArrow1 size={30} className='services-item-arrow' />
                     </div>
-                    <div className='services-div-1-1'>
-                        <div className='services-div-1-1-1'>04 
-                             <CiLocationArrow1 size={30} className='services-div-1-arrow'/>
-                        </div>
-                        <div className='services-div-1-1-2'>SEO</div>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, facere!</p>
+                    <div className='services-item-title'>BACKEND DEVELOPER</div>
+                    <p>Lorem ipsum dolor sit amet consectetur, iusto corporis at sunt officia! Itaque, deleniti qui!</p>
+                </div>
+                <div className='services-item'>
+                    <div className='services-item-header'>
+                        03
+                        <CiLocationArrow1 size={30} className='services-item-arrow' />
                     </div>
+                    <div className='services-item-title'>COMPETITIVE PROGRAMMER</div>
+                    <p>Lorem ipsum dolor sit amet consectetur, iusto corporis at sunt officia! Itaque, deleniti qui!</p>
+                </div>
+                <div className='services-item'>
+                    <div className='services-item-header'>
+                        04
+                        <CiLocationArrow1 size={30} className='services-item-arrow' />
+                    </div>
+                    <div className='services-item-title'>SEO</div>
+                    <p>Lorem ipsum dolor sit amet consectetur, iusto corporis at sunt officia! Itaque, deleniti qui!</p>
                 </div>
             </div>
         </div>
@@ -45,3 +45,4 @@ function Services() {
 }
 
 export default Services;
+
